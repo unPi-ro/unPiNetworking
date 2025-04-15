802bd92807cb49c69bcff7d954a4a319c4beccb1
Vlad <vlad@unpi.dev>
2025-04-15 19:38:54 +0300

    Linter

--- Tests/unPiNetworkingTests/unPiNetworkingTests.swift
+++ Tests/unPiNetworkingTests/unPiNetworkingTests.swift
@@ -3,20 +3,20 @@ import XCTest
 
 final class URLSessionTests: XCTestCase {
     private var session: URLSession!
-    
+
     override func setUp() {
         super.setUp()
         let configuration = URLSessionConfiguration.ephemeral
         configuration.protocolClasses = [URLProtocolStub.self]
         session = URLSession(configuration: configuration)
     }
-    
+
     override func tearDown() {
         session = nil
         URLProtocolStub.requestHandler = nil
         super.tearDown()
     }
-    
+
     func test_successfulRequest_returnsExpectedData() async throws {
         // Given
         let expectedData = "Test response".data(using: .utf8)!
@@ -26,11 +26,11 @@ final class URLSessionTests: XCTestCase {
             httpVersion: nil,
             headerFields: nil
         )!
-        
+
         URLProtocolStub.requestHandler = { request in
             return (expectedResponse, expectedData)
         }
-        
+
         // When
         let (data, response) = try await session.data(from: URL(string: "https://example.com")!)
         
@@ -38,14 +38,14 @@ final class URLSessionTests: XCTestCase {
         XCTAssertEqual(data, expectedData)
         XCTAssertEqual((response as? HTTPURLResponse)?.statusCode, 200)
     }
-    
+
     func test_failedRequest_throwsError() async {
         // Given
         let expectedError = NSError(domain: "TestError", code: 1)
         URLProtocolStub.requestHandler = { _ in
             throw expectedError
         }
-        
+
         // When/Then
         do {
             _ = try await session.data(from: URL(string: "https://example.com")!)
@@ -56,7 +56,7 @@ final class URLSessionTests: XCTestCase {
             XCTAssertEqual(nsError.code, expectedError.code)
         }
     }
-    
+
     func test_requestWithCustomHeaders_includesHeaders() async throws {
         // Given
         let expectedHeaders = ["Authorization": "Bearer token"]
@@ -66,17 +66,16 @@ final class URLSessionTests: XCTestCase {
             capturedRequest = request
             return (HTTPURLResponse(), Data())
         }
-        
+
         // When
         var request = URLRequest(url: URL(string: "https://example.com")!)
         request.allHTTPHeaderFields = expectedHeaders
         _ = try await session.data(for: request)
-        
+
         // Then
         XCTAssertEqual(capturedRequest?.allHTTPHeaderFields, expectedHeaders)
     }
 }
-
 
 final class URLProtocolStub: URLProtocol {
     nonisolated(unsafe) static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
@@ -84,17 +83,17 @@ final class URLProtocolStub: URLProtocol {
     override class func canInit(with request: URLRequest) -> Bool {
         return true
     }
-    
+
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
-    
+
     override func startLoading() {
         guard let handler = URLProtocolStub.requestHandler else {
             XCTFail("Received unexpected request with no handler set")
             return
         }
-        
+
         do {
             let (response, data) = try handler(request)
             client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
@@ -104,6 +103,6 @@ final class URLProtocolStub: URLProtocol {
             client?.urlProtocol(self, didFailWithError: error)
         }
     }
-    
+
     override func stopLoading() {}
 }
